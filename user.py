a310e39f5edda5b207a40e62de267cfa309f169a
名字 <>
2024-05-07 12:05:05 +0800

    test finished

--- user.py
+++ user.py
@@ -13,200 +13,200 @@ from database import db
 
 
 
-user = Blueprint("user", __name__,)
+user = Blueprint("user",__name__)
 
-@user.route('/user/get_user_name',methods=['GET', 'POST'])
+
+#ok
+@user.route('/get_name',methods=['GET', 'POST'])
 def get_user_name():
-    
-    user_Id = request.args.get('user_Id')
-    #user_Id = 3
+    user_id = request.args.get('user_id')
     cursor = db.connection.cursor()
     cursor.execute(
         f"""
             SELECT UName
             FROM users 
-            WHERE UID = {user_Id};
+            WHERE UID = {user_id};
         """
     )
     result = cursor.fetchone()
     cursor.close()
     return jsonify(result)
 
-@user.route('/user/get_is_right',methods=['GET', 'POST'])
+@user.route('/get_isright',methods=['GET', 'POST'])
 def get_is_right():
     
-    user_Id = request.args.get('user_Id')
+    user_id = request.args.get('user_id')
     cursor = db.connection.cursor()
     cursor.execute(
         f"""
             SELECT isRightHander
             FROM users 
-            WHERE UID = {user_Id};
+            WHERE UID = {user_id};
         """
     )
     result = cursor.fetchone()
     cursor.close()
-    return result
+    return jsonify(result)
 
-@user.route('/user/get_is_dark',methods=['GET', 'POST'])
+@user.route('/get_isdark',methods=['GET', 'POST'])
 def get_is_dark():
     
-    user_Id = request.args.get('user_Id')
+    user_id = request.args.get('user_id')
     cursor = db.connection.cursor()
     cursor.execute(
         f"""
             SELECT isDarkMode
             FROM users 
-            WHERE UID = {user_Id};
+            WHERE UID = {user_id};
         """
     )
     result = cursor.fetchone()
     cursor.close()
-    return result
+    return jsonify(result)
 
-@user.route('/user/get_ntime',methods=['GET', 'POST'])
+@user.route('/get_ntime',methods=['GET', 'POST'])
 def get_ntime():
     
-    user_Id = request.args.get('user_Id')
+    user_id = request.args.get('user_id')
     cursor = db.connection.cursor()
     cursor.execute(
         f"""
             SELECT NoticeTime
             FROM users 
-            WHERE UID = {user_Id};
+            WHERE UID = {user_id};
         """
     )
     result = cursor.fetchone()
     cursor.close()
-    return result
+    return jsonify(result)
 
-@user.route('/user/get_user_acc',methods=['GET', 'POST'])
+@user.route('/get_acc',methods=['GET', 'POST'])
 def get_user_acc():
     
-    user_Id = request.args.get('user_Id')
+    user_id = request.args.get('user_id')
     cursor = db.connection.cursor()
     cursor.execute(
         f"""
             SELECT UAccount
             FROM users 
-            WHERE UID = {user_Id};
+            WHERE UID = {user_id};
         """
     )
     result = cursor.fetchone()
     cursor.close()
-    return result
+    return jsonify(result)
 
-@user.route('/user/get_user_password',methods=['GET', 'POST'])
+@user.route('/get_password',methods=['GET', 'POST'])
 def get_user_password():
     
-    user_Id = request.args.get('user_Id')
+    user_id = request.args.get('user_id')
     cursor = db.connection.cursor()
     cursor.execute(
         f"""
             SELECT UPassword
             FROM users 
-            WHERE UID = {user_Id};
+            WHERE UID = {user_id};
         """
     )
     result = cursor.fetchone()
     cursor.close()
-    return result
+    return jsonify(result)
 
-@user.route('/user/get_budget',methods=['GET', 'POST'])
+@user.route('/get_budget',methods=['GET', 'POST'])
 def get_budget():
     
-    user_Id = request.args.get('user_Id')
+    user_id = request.args.get('user_id')
     cursor = db.connection.cursor()
     cursor.execute(
         f"""
             SELECT Budget
             FROM users 
-            WHERE UID = {user_Id};
+            WHERE UID = {user_id};
         """
     )
     result = cursor.fetchone()
     cursor.close()
-    return result
+    return jsonify(result)
 
-@user.route('/user/get_user_nname',methods=['GET', 'POST'])
+@user.route('/get_nname',methods=['GET', 'POST'])
 def get_user_nname():
     
-    user_Id = request.args.get('user_Id')
+    user_id = request.args.get('user_id')
     cursor = db.connection.cursor()
     cursor.execute(
         f"""
             SELECT UNickname
             FROM users 
-            WHERE UID = {user_Id};
+            WHERE UID = {user_id};
         """
     )
     result = cursor.fetchone()
     cursor.close()
-    return result
+    return jsonify(result)
 
 
-@user.route('/user/update_user_name',methods=['PUT', 'POST'])
+@user.route('/update_name',methods=['PUT', 'GET'])
 def update_user_name():
     
-    user_Id = request.args.get('user_Id')
+    user_id = request.args.get('user_id')
     new_name = request.args.get('new_name')
     cursor = db.connection.cursor()
     cursor.execute(
         f"""
             update users
-            set UName = {new_name}
-            WHERE UID = {user_Id};
+            set UName = '{new_name}'
+            WHERE UID = {user_id};
         """
     )
     db.connection.commit()
     cursor.close()
     return "Update successfully!"
 
-@user.route('/user/update_user_password',methods=['PUT', 'POST'])
+@user.route('/update_password',methods=['PUT', 'GET'])
 def update_user_password():
     
-    user_Id = request.args.get('user_Id')
+    user_id = request.args.get('user_id')
     new_password = request.args.get('new_password')
     cursor = db.connection.cursor()
     cursor.execute(
         f"""
             update users
-            set UName = {new_password}
-            WHERE UID = {user_Id};
+            set UPassword = {new_password}
+            WHERE UID = {user_id};
         """
     )
     db.connection.commit()
     cursor.close()
     return "Update successfully!"
 
-@user.route('/user/update_user_acc',methods=['PUT', 'POST'])
+@user.route('/update_acc',methods=['PUT', 'GET'])
 def update_user_acc():
     
-    user_Id = request.args.get('user_Id')
+    user_id = request.args.get('user_id')
     new_acc = request.args.get('new_acc')
     cursor = db.connection.cursor()
     cursor.execute(
         f"""
             update users
-            set UName = {new_acc}
-            WHERE UID = {user_Id};
+            set Uaccount = {new_acc}
+            WHERE UID = {user_id};
         """
     )
     db.connection.commit()
     cursor.close()
     return "Update successfully!"
 
-@user.route('/user/update_user_nname',methods=['PUT', 'POST'])
+@user.route('/update_nname',methods=['PUT', 'GET'])
 def update_user_nname():
     
-    user_Id = request.args.get('user_Id')
+    user_id = request.args.get('user_id')
     new_nname = request.args.get('new_nname')
     cursor = db.connection.cursor()
     cursor.execute(
         f"""
             update users
-            set UName = {new_nname}
-            WHERE UID = {user_Id};
+            set Unickname = '{new_nname}'
+            WHERE UID = {user_id};
         """
     )
     db.connection.commit()
@@ -214,17 +214,17 @@ def update_user_nname():
     return "Update successfully!"
 
 
-@user.route('/user/update_user_ntime',methods=['PUT', 'POST'])
+@user.route('/update_ntime',methods=['PUT', 'GET'])
 def update_user_ntime():
     
-    user_Id = request.args.get('user_Id')
+    user_id = request.args.get('user_id')
     new_ntime = request.args.get('new_ntime')
     cursor = db.connection.cursor()
     cursor.execute(
         f"""
             update users
-            set UName = {new_ntime}
-            WHERE UID = {user_Id};
+            set Noticetime = {new_ntime}
+            WHERE UID = {user_id};
         """
     )
     db.connection.commit()
@@ -232,34 +232,34 @@ def update_user_ntime():
     return "Update successfully!"
 
 
-@user.route('/user/update_user_isright',methods=['PUT', 'POST'])
+@user.route('/update_isright',methods=['PUT', 'GET'])
 def update_user_isright():
     
-    user_Id = request.args.get('user_Id')
+    user_id = request.args.get('user_id')
     new_isright = request.args.get('new_isright')
     cursor = db.connection.cursor()
     cursor.execute(
         f"""
             update users
-            set UName = {new_isright}
-            WHERE UID = {user_Id};
+            set isrighthander = {new_isright}
+            WHERE UID = {user_id};
         """
     )
     db.connection.commit()
     cursor.close()
     return "Update successfully!"
 
-@user.route('/user/update_user_isdark',methods=['PUT', 'POST'])
+@user.route('/update_isdark',methods=['PUT', 'GET'])
 def update_user_isdark():
     
-    user_Id = request.args.get('user_Id')
+    user_id = request.args.get('user_id')
     new_isdark = request.args.get('new_isdark')
     cursor = db.connection.cursor()
     cursor.execute(
         f"""
             update users
-            set UName = {new_isdark}
-            WHERE UID = {user_Id};
+            set isdarkmode = {new_isdark}
+            WHERE UID = {user_id};
         """
     )
     db.connection.commit()
@@ -267,34 +267,17 @@ def update_user_isdark():
     return "Update successfully!"
 
 
-@user.route('/user/update_user_budget',methods=['PUT', 'POST'])
+@user.route('/update_budget',methods=['PUT', 'GET'])
 def update_user_budget():
     
-    user_Id = request.args.get('user_Id')
+    user_id = request.args.get('user_id')
     new_budget = request.args.get('new_budget')
     cursor = db.connection.cursor()
     cursor.execute(
         f"""
             update users
-            set UName = {new_budget}
-            WHERE UID = {user_Id};
-        """
-    )
-    db.connection.commit()
-    cursor.close()
-    return "Update successfully!"
-
-@user.route('/user/update_user_name',methods=['PUT', 'POST'])
-def update_user_name():
-    
-    user_Id = request.args.get('user_Id')
-    new_name = request.args.get('new_name')
-    cursor = db.connection.cursor()
-    cursor.execute(
-        f"""
-            update users
-            set UName = {new_name}
-            WHERE UID = {user_Id},
+            set budget = {new_budget}
+            WHERE UID = {user_id};
         """
     )
     db.connection.commit()
@@ -302,7 +285,9 @@ def update_user_name():
     return "Update successfully!"
 
 
-@user.route('/user/insert_user_acc_password',methods=['PUT', 'POST'])
+
+
+@user.route('/insert_acc_password',methods=['GET','POST'])
 def insert_user_acc_password():
     
     user_name = request.args.get('user_name')
@@ -316,8 +301,6 @@ def insert_user_acc_password():
     db.connection.commit()
     return "insert successfully!"
 
-if __name__ == '__main__':
-    user.run(debug=True)
 
 
 
