4ebe27c003b4012b1e7eec4bf522fa6f1e83ccc4
Mason-Hsun <mason.hsun@gmail.com>
2024-05-10 14:37:23 +0800

    Final version of DB and API test passed

--- flaskr/app.py
+++ flaskr/app.py
@@ -2,13 +2,13 @@ import os
 from database import db
 from dotenv import load_dotenv
 from flask import Flask, jsonify
-
-
-
+from data import data
+from user import user
+from ledger import ledger
 app = Flask(__name__)
-load_dotenv()
-db.init_app(app)
 
+
+load_dotenv()
 
 app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
 app.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT"))
@@ -16,8 +16,11 @@ app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
 app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
 app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
 
+db.init_app(app)
 
-
+app.register_blueprint(user, url_prefix="/user")
+app.register_blueprint(ledger, url_prefix="/ledger")
+app.register_blueprint(data, url_prefix="/data")
 
 if __name__ == "__main__":
     app.run(debug=True)
