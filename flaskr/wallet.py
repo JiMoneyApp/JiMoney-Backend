4e8dd1a94b2b81b143ec557919c75cce0f8411b4
名字 <>
2024-06-19 12:33:21 +0800

    in wallet.py add insert,delete,update wallet and get all wallets

--- flaskr/wallet.py
+++ flaskr/wallet.py
@@ -41,3 +41,93 @@ def get_all_wallets():
         cursor.close()
         abort(500, "ERROR 500")
 
+@wallet.route('/insert_wallet',methods=['GET', 'POST', 'PUT'])
+def insert_wallet():
+
+    user_id = request.args.get('user_id')
+    wallet_name = request.args.get('wallet_name')
+    cursor = db.connection.cursor()
+    try:
+        cursor.execute(
+            f"""
+                INSERT INTO Wallets (UID, WName)
+                VALUES ({user_id}, '{wallet_name}');
+            """
+        )
+        cursor.execute("COMMIT")
+        cursor.close()
+        return "success",201
+    except:
+        cursor.execute("ROLLBACK")
+        cursor.close()
+        abort(500, "ERROR 500")
+
+@wallet.route('/delete_wallet',methods=['GET', 'POST', 'DELETE'])
+def delete_wallet():
+
+    wallet_id = request.args.get('wallet_id')
+    cursor = db.connection.cursor()
+    try:
+        cursor.execute(
+            f"""
+                DELETE FROM Wallets
+                WHERE WID = {wallet_id};
+            """
+        )
+        cursor.execute("COMMIT")
+        cursor.close()
+        return "success",201
+    except:
+        cursor.execute("ROLLBACK")
+        cursor.close()
+        abort(500, "ERROR 500")
+
+@wallet.route('/update_wallet',methods=['GET', 'POST', 'PUT'])
+def update_wallet():
+
+    wallet_id = request.args.get('wallet_id')
+    new_wallet_name = request.args.get('new_wallet_name')
+    cursor = db.connection.cursor()
+    try:
+        cursor.execute(
+            f"""
+                UPDATE Wallets
+                SET WName = '{new_wallet_name}'
+                WHERE WID = {wallet_id};
+            """
+        )
+        cursor.execute("COMMIT")
+        cursor.close()
+        return "success",201
+    except:
+        cursor.execute("ROLLBACK")
+        cursor.close()
+        abort(500, "ERROR 500")
+
+@wallet.route('/get_all_wallets',methods=['GET', 'POST'])
+def get_all_wallets():
+
+    user_id = request.args.get('user_id')
+    cursor = db.connection.cursor()
+    try:
+        cursor.execute(
+            f"""
+                SELECT WID, WName
+                FROM Wallets
+                WHERE UID = {user_id};
+            """
+        )
+        result = cursor.fetchall()
+        wallets = []
+        for item in result:
+            wid, wname = item
+            wallets.append({
+                "WID": wid,
+                "WNAME": wname
+            })
+        cursor.close()
+        return jsonify(wallets),200
+    except:
+        cursor.execute("ROLLBACK")
+        cursor.close()
+        abort(500, "ERROR 500")
