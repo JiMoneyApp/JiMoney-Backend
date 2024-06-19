8a105d8d5d576455c2e00df219fb2299705b4465
名字 <>
2024-06-19 13:12:31 +0800

    update test_app

--- flaskr/test_app.py
+++ flaskr/test_app.py
@@ -5,30 +5,55 @@ class TestCase:
         self.base_url = 'http://localhost:5000'
 
     def test_get_all_wallets(self):
-        suffix = '/home_page/get_all_wallets?user_id=1'
+        suffix = '/wallet/get_all_wallets?user_id=1'
         response = requests.get(self.base_url + suffix)
         print(response.status_code)
         print(response.json())  # 打印回應的JSON內容
 
     def test_get_all_ledgers(self):
-        suffix = '/home_page/get_all_ledgers?wallet_id=1'
+        suffix = '/ledger/get_ledgers?wallet_id=1'
         response = requests.get(self.base_url + suffix)
         print(response.status_code)
         print(response.json())
 
     def test_get_all_goals(self):
-        suffix = '/home_page/get_all_goals?user_id=1'
+        suffix = '/goal/get_all_goals?user_id=1'
         response = requests.get(self.base_url + suffix)
         print(response.status_code)
         print(response.json())
 
     def test_get_my_partner_goal(self):
-        suffix = '/home_page/get_my_partner_goal?data_id=1'
+        suffix = '/goal/get_my_partner_goal?data_id=1'
         response = requests.get(self.base_url + suffix)
         print(response.status_code)
         print(response.json())
+
     def test_get_my_partner_ledger(self):
-        suffix = '/home_page/get_my_partner_ledger?data_id=1'
+        suffix = '/ledger/get_my_partner_ledger?data_id=1'
+        response = requests.get(self.base_url + suffix)
+        print(response.status_code)
+        print(response.json())
+
+    def test_insert_wallet(self):
+        suffix = '/wallet/insert_wallet?user_id=1&wallet_name=wallet1'
+        response = requests.post(self.base_url + suffix)
+        print(response.status_code)
+        print(response.json())
+
+    def test_delete_wallet(self):
+        suffix = '/wallet/delete_wallet?wallet_id=1'
+        response = requests.delete(self.base_url + suffix)
+        print(response.status_code)
+        print(response.json())
+
+    def test_update_wallet(self):
+        suffix = '/wallet/update_wallet?wallet_id=2&new_wallet_name=fuckyou'
+        response = requests.put(self.base_url + suffix)
+        print(response.status_code)
+        print(response.json())
+
+    def test_get_wallet_all_ledger(self):
+        suffix = '/wallet/get_wallet_all_ledger?wallet_id=1'
         response = requests.get(self.base_url + suffix)
         print(response.status_code)
         print(response.json())
@@ -40,6 +65,10 @@ def main():
     test.test_get_all_goals()
     test.test_get_my_partner_goal()
     test.test_get_my_partner_ledger()
+    test.test_insert_wallet()
+    test.test_delete_wallet()
+    test.test_update_wallet()
+    test.test_get_wallet_all_ledger()
 
 
 if __name__ == "__main__":
